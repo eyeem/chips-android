7121d2f7c17764893a3fb9dc737650b79d9ae6fd
Lukasz Wisniewski <luke.cherrish@gmail.com>
2013-05-13 18:40:36 +0200

    Initial commit

---
+++ src/com/eyeem/chips/AutocompletePopover.java
@@ -0,0 +1,154 @@
+package com.eyeem.chips;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.*;
+
+import java.util.ArrayList;
+
+public class AutocompletePopover extends RelativeLayout {
+
+   ListView lv;
+   RelativeLayout root;
+   ChipsEditText et;
+   Adapter adapter = new Adapter();
+   View tri;
+
+   public AutocompletePopover(Context context) {
+      super(context);
+      init();
+   }
+
+   public AutocompletePopover(Context context, AttributeSet attrs) {
+      super(context, attrs);
+      init();
+   }
+
+   public AutocompletePopover(Context context, AttributeSet attrs, int defStyle) {
+      super(context, attrs, defStyle);
+      init();
+   }
+
+   void init() {
+      LayoutInflater.from(getContext()).inflate(R.layout.autocomplete_popover, this, true);
+      lv = (ListView)findViewById(R.id.suggestions);
+      tri = findViewById(R.id.triangle);
+      lv.setAdapter(adapter);
+      lv.setOnItemClickListener(onItemClickListener);
+      setVisibility(View.GONE);
+   }
+
+   public void setChipsEditText(ChipsEditText et) {
+      this.et = et;
+   }
+
+   public void setItems(ArrayList<String> items) {
+      adapter.setItems(items);
+   }
+
+   public RelativeLayout root() {
+      if (root == null) {
+         root = (RelativeLayout) getParent();
+         root.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+               reposition();
+            }
+         });
+      }
+      return root;
+   }
+
+   public void reposition() {
+      Point p = et.getCursorPosition();
+      int verticaloffset = -(int)(et.getHeight() - p.y - 2*et.getTextSize());
+      int h = root().getHeight() - et.getHeight() - verticaloffset;
+      ((RelativeLayout.LayoutParams)getLayoutParams()).topMargin = et.getHeight() + verticaloffset;
+      getLayoutParams().height = h;
+      ((LayoutParams)tri.getLayoutParams()).leftMargin = p.x;
+   }
+
+   public void show() {
+      reposition();
+      setVisibility(View.VISIBLE);
+   }
+
+   public void hide() {
+      setVisibility(View.GONE);
+   }
+
+   public static class Adapter extends BaseAdapter {
+
+      ArrayList<String> items = new ArrayList<String>();
+      LayoutInflater li;
+
+      public Adapter() {
+         //Resources r = ApplicationController.instance().getResources();
+         //_16dp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, r.getDisplayMetrics());
+      }
+
+      public void setItems(ArrayList<String> items) {
+         this.items = items;
+         notifyDataSetChanged();
+      }
+
+      @Override public int getCount() { return items.size(); }
+      @Override public Object getItem(int position) { return items.get(position); }
+      @Override public long getItemId(int position) { return position; }
+
+      @Override
+      public View getView(int position, View c, ViewGroup parent) {
+         ((ListView)parent).setBackgroundColor(0x77000000);
+         ((ListView)parent).setDivider(null);
+         ((ListView)parent).setSelector(new ColorDrawable(0x0));
+         // boilerplate code
+         if (li == null) {
+            li = LayoutInflater.from(parent.getContext());
+         }
+         ViewHolder h;
+         if (c == null) {
+            h = new ViewHolder();
+            c = li.inflate(R.layout.autocomplete_row, null);
+            h.title = (TextView)c.findViewById(R.id.title);
+            c.setTag(h);
+         } else {
+            h = (ViewHolder)c.getTag();
+         }
+         String text = items.get(position);
+         // attach data
+         h.title.setText(text);
+
+         return c;
+      }
+
+      public static class ViewHolder {
+         TextView title;
+      }
+   }
+
+   public AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+         et.manualModeOn = false;
+         String textToAdd = adapter.items.get(position);
+         if (et.lastEditAction != null) {
+            try {
+               Editable t = et.getText();
+               if (t.subSequence(et.lastEditAction.start, et.lastEditAction.end()).toString().equals(et.lastEditAction.text)) {
+                  t.delete(et.lastEditAction.start, et.lastEditAction.end());
+               }
+            } catch (Exception e) {}
+         }
+         et.addBubble(textToAdd, et.getSelectionStart());
+         hide();
+      }
+   };
+}
---
+++ src/com/eyeem/chips/ChipsEditText.java
@@ -0,0 +1,232 @@
+package com.eyeem.chips;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.text.*;
+import android.text.style.ImageSpan;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ChipsEditText extends EditText {
+
+   ArrayList<String> availableItems = new ArrayList<String>();
+   ArrayList<String> filteredItems = new ArrayList<String>();
+   EditAction lastEditAction;
+   AutocompletePopover popover;
+
+   public ChipsEditText(Context context) {
+      super(context);
+      init();
+   }
+
+   public ChipsEditText(Context context, AttributeSet attrs) {
+      super(context, attrs);
+      init();
+   }
+
+   public ChipsEditText(Context context, AttributeSet attrs, int defStyle) {
+      super(context, attrs, defStyle);
+      init();
+   }
+
+   void init() {
+      addTextChangedListener(autocompleteWatcher);
+      setOnEditorActionListener(editorActionListener);
+   }
+
+   public void setAutocomplePopover(AutocompletePopover popover) {
+      this.popover = popover;
+   }
+
+   public void makeChip(int start, int end) {
+      String text = getText().toString();
+      text = text.substring(start, end);
+
+      // inflate chips_edittext layout
+      LayoutInflater lf = (LayoutInflater) getContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+      TextView textView = (TextView) lf.inflate(R.layout.chips, null);
+      textView.setText(text); // set text
+      // capture bitmapt of genreated textview
+      int spec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+      textView.measure(spec, spec);
+      textView.layout(0, 0, textView.getMeasuredWidth(), textView.getMeasuredHeight());
+      Bitmap b = Bitmap.createBitmap(textView.getWidth(), textView.getHeight(), Bitmap.Config.ARGB_8888);
+      Canvas canvas = new Canvas(b);
+      canvas.translate(-textView.getScrollX(), -textView.getScrollY());
+      textView.draw(canvas);
+      textView.setDrawingCacheEnabled(true);
+      Bitmap cacheBmp = textView.getDrawingCache();
+      Bitmap viewBmp = cacheBmp.copy(Bitmap.Config.ARGB_8888, true);
+      textView.destroyDrawingCache();  // destory drawable
+      // create bitmap drawable for imagespan
+      BitmapDrawable bmpDrawable = new BitmapDrawable(viewBmp);
+      bmpDrawable.setBounds(0, 0, bmpDrawable.getIntrinsicWidth(), bmpDrawable.getIntrinsicHeight());
+      // create and set imagespan
+      ImageSpan[] spansToClear = getText().getSpans(start, end, ImageSpan.class);
+      for (ImageSpan span : spansToClear)
+         getText().removeSpan(span);
+      getText().setSpan(new ImageSpan(bmpDrawable), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+   }
+
+   public void addBubble(String text, int start) {
+      getText().insert(start, text);
+      makeChip(start, start+text.length());
+   }
+
+   @Override
+   public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+      InputConnection connection = super.onCreateInputConnection(outAttrs);
+      int imeActions = outAttrs.imeOptions&EditorInfo.IME_MASK_ACTION;
+      if ((imeActions&EditorInfo.IME_ACTION_DONE) != 0) {
+         // clear the existing action
+         outAttrs.imeOptions ^= imeActions;
+         // set the DONE action
+         outAttrs.imeOptions |= EditorInfo.IME_ACTION_DONE;
+      }
+      if ((outAttrs.imeOptions&EditorInfo.IME_FLAG_NO_ENTER_ACTION) != 0) {
+         outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
+      }
+      return connection;
+   }
+
+   boolean manualModeOn;
+   int manualStart;
+
+   public void startManualMode() {
+      manualModeOn = true;
+      manualStart = getSelectionStart();
+   }
+
+   public void endManualMode() {
+      makeChip(manualStart, getSelectionEnd());
+      manualModeOn = false;
+   }
+
+   TextWatcher autocompleteWatcher = new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+         String textForAutocomplete = null;
+         try {
+            textForAutocomplete = s.toString().substring(start, start+count);
+            if (!TextUtils.isEmpty(textForAutocomplete)) {
+               showAutocomplete(new EditAction(textForAutocomplete, start, before, count));
+            }
+         } catch (Exception e) {
+         }
+      }
+
+      @Override
+      public void afterTextChanged(Editable s) {
+         if (manualModeOn) {
+            int end = getSelectionStart();
+            if (end < manualStart) {
+               manualModeOn = false;
+            } else {
+               makeChip(manualStart, end);
+            }
+         }
+         popover.reposition();
+      }
+   };
+
+   public void setAvailableItems(ArrayList<String> items) {
+      availableItems = items;
+      filter();
+   }
+
+   private void filter() {
+      filteredItems.clear();
+      if (lastEditAction != null) {
+         String text = lastEditAction.text.toLowerCase();
+         for (String item : availableItems) {
+            if ((text.length() > 1 && item.toLowerCase().startsWith(text)) || (item.toLowerCase().contains(text) && text.length() > 3)) {
+               filteredItems.add(item);
+            }
+         }
+      }
+      if (filteredItems.size() > 0) {
+         popover.setItems(filteredItems);
+         popover.show();
+      } else {
+         if (!manualModeOn) {
+            popover.hide();
+         }
+         popover.setItems(availableItems);
+      }
+   }
+
+   public void showAutocomplete(EditAction editAction) {
+      lastEditAction = editAction;
+      filter();
+   }
+
+   TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
+      @Override
+      public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+         if (actionId == EditorInfo.IME_ACTION_DONE && manualModeOn) {
+            endManualMode();
+            return true;
+         } else if (actionId == EditorInfo.IME_ACTION_DONE && !manualModeOn) {
+            hideKeyboard();
+            return true;
+         }
+         return false;
+      }
+   };
+
+   public void hideKeyboard() {
+      InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+      imm.hideSoftInputFromWindow(getWindowToken(), 0);
+      popover.hide();
+   }
+
+   public void showKeyboard() {
+      InputMethodManager inputMgr = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+      //inputMgr.toggleSoftInput(InputMethodManager.SHOW_, 0);
+      inputMgr.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT);
+   }
+
+   public Point getCursorPosition() {
+      int pos = getSelectionStart();
+      Layout layout = getLayout();
+      int line = layout.getLineForOffset(pos);
+      int baseline = layout.getLineBaseline(line);
+      int ascent = layout.getLineAscent(line);
+      float x = layout.getPrimaryHorizontal(pos);
+      float y = baseline + ascent;
+      return new Point((int)x, (int)y);
+   }
+
+   public static class EditAction {
+      String text;
+      int start;
+      int before;
+      int count;
+      int end() {
+         return start + count;
+      }
+
+      public EditAction(String text, int start, int before, int count) {
+         this.text = text;
+         this.start = start;
+         this.before = before;
+         this.count = count;
+      }
+   }
+}
